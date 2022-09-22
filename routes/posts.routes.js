c0cc6e3a6b77e993f94b75afe017529e21993d55
Clement Lecocq <hello@clementlecocq.com>
2022-09-22 14:49:11 +0200

    (ROUTES) MAJ routes crud

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -4,10 +4,16 @@ const router = express.Router();
 // Controlleurs et middlewaires
 
 const postCtrl = require('../controllers/posts.controller');
+const auth = require('../middleware/auth.middleware');
+const multer = require("../middleware/multerFunctions");
+
 
 // Routes CRUD
 
+router.post("/trending", auth, multer, postCtrl.createPost);
 router.get('/trending', auth, postCtrl.displayPosts);
+router.delete("/trending/:id", auth, postCtrl.deletePost);
+router.put("/trending/:id", auth, multer, postCtrl.modifyPost);
 
 
 module.exports = router;

a969808626f81d8cb4cf8b19976e649779f1ab5c
Clement Lecocq <hello@clementlecocq.com>
2022-09-22 14:30:31 +0200

    (CRUS) Ajout controllers post : create / modify / delete

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -6,7 +6,68 @@ const fs = require('fs');
 // Afficher toutes les posts
 
 exports.displayPosts = (req, res, next) => {
-    Sauce.find()
+    Post.find()
         .then(posts => res.status(200).json(posts))
         .catch(error => res.status(400).json({ error }));
+};
+
+// Créer un post
+
+exports.createPost = (req, res, next) => {
+    const postObject = JSON.parse(req.body.post);
+    delete postObject._id;
+    const post = new Post({
+        ...postObject,
+        imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    post.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré' }))
+        .catch(error => res.status(400).json({ error }));
+};
+
+// Modifier un post
+
+exports.modifySauce = (req, res, next) => {
+    const postObject = req.file ? {
+        ...JSON.parse(req.body.post),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body };
+
+    delete postObject._userId;
+    Post.findOne({_id: req.params.id})
+        .then((post) => {
+            if (post == null) {
+                res.status(404).json({ message: "Le post n'existe pas" });
+            } else if (post.userId != req.auth.userId) {
+                res.status(403).json({ message: "Vous n'êtes pas autorisé à modifier ce post" });
+            } else {
+                const filename = post.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Post.updateOne({ _id: req.params.id }, {...postObject, _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Objet supprimé' }))
+                        .catch(error => res.status(400).json({ error }));
+                });
+            }
+        });
+}
+
+// Supprimer un post
+
+exports.deletePost = (req, res, next) => {
+    Post.findOne({ _id: req.params.id })
+        .then(post => {
+            if (post == null) {
+                res.status(404).json({ message: "Le post n'existe pas" });
+            } else if (post.userId != req.auth.userId) {
+                res.status(403).json({ message: "Vous n'êtes pas autorisé à supprimer le post" });
+            } else {
+                const filename = post.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Post.deleteOne({ _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Objet supprimé' }))
+                        .catch(error => res.status(400).json({ error }));
+                });
+            }
+        })
+        .catch(error => res.status(500).json({ error }));
 };

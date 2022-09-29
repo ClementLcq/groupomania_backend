8e87cb7cde2fb7b797d6b2de9ccc94aeb2515524
Clement Lecocq <hello@clementlecocq.com>
2022-09-29 12:45:18 +0200

    MAJ post schema

--- models/Post.js
+++ models/Post.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 // Création du schéma Post attendu par le front
 
 const postSchema = mongoose.Schema({
-    userEmail: { type: String, required: true },
-   description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    likes: { type: Number, required: false, default: 0 },
-    usersLiked: { type: [String] },
+        userId: { type: String, required: true },
+        description: { type: String, required: true },
+        imageUrl: { type: String, required: true },
+        likes: { type: Number, required: false, default: 0 },
+        usersLiked: { type: [String] },
     }, 
     { timestamps: true }
 );

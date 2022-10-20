fc636574f54f7c53be12324dc43f178e10702085
Clement Lecocq <hello@clementlecocq.com>
2022-10-20 18:11:27 +0200

    (RAS)

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -129,6 +129,25 @@ exports.deletePost = (req, res, next) => {
 };
 
 // liker un post
+
+// exports.likePost = async (req, res) => {
+//     //Get the post with params
+//     const post = await Post.findById(req.params.id);
+//     if (req.auth.userId !== post.userId) {
+//       //Check if user allready like the post, if not, insert userId in likes array
+//       //else pull-it from likes array
+//       if (!post.likes.includes(req.auth.userId)) {
+//         await post.updateOne({ $push: { likes: req.auth.userId } });
+//         res.status(200).json("Like enregistré !");
+//       } else {
+//         await post.updateOne({ $pull: { likes: req.auth.userId } });
+//         res.status(200).json("Like retiré !");
+//       }
+//     } else {
+//       res.status(401).json("Vous ne pouvez pas liker vos posts !");
+//     }
+//   };
+
 exports.likePost = (req, res, next) => {
     // Récupération du post avec params.id
     Post.findOne({ _id: req.params.id })

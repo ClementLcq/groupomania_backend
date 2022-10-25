f160770ea2f4cb39f6a5bafb7bb42c6bc1bfacd3
Clement Lecocq <hello@clementlecocq.com>
2022-10-25 11:35:57 +0200

    (PUT CONTROLLER) MAJ ok

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -49,63 +49,45 @@ exports.createPost = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
-
-// exports.createPost = (req, res, next) => {
-//     // console.log(JSON.parse(JSON.stringify(req.body)));
-//     const content = req.body.post;
-//     const post = new Post({
-//         userId : req.auth.userId,
-//         content : content,
-//         imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-//     });
-//     post.save()
-//         .then(() => res.status(201).json({ message: 'Objet enregistré' }))
-//         .catch(error => res.status(400).json({ error }));
-// };
-
-// exports.createPost = async (req, res) => {
-//     const { userId, description } = req.body;
-//     const imageURL = req.file
-//       ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
-//       : "";
-//     await Post.create({
-//       userId: userId,
-//       description: description,
-//       image: imageURL,
-//     })
-//       .then(() => {
-//         res.status(201).json({ message: "Nouveau Post créé !" });
-//       })
-//       .catch((error) => {
-//         res.status(400).json({ error });
-//       });
-//   };
-
 // Modifier un post
 
-exports.modifyPost = (req, res, next) => {
-    const postObject = req.file ? {
-        ...JSON.parse(req.body.post),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body };
-
-    delete postObject._userId;
-    Post.findOne({_id: req.params.id})
+exports.modifyPost = async (req, res) => {
+    const { description } = req.body;
+    const imageUrl = req.file
+      ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+      : "";
+    // On regarde si il y a un fichier dans le post
+    const postObject = {
+      description,
+      imageUrl: imageUrl,
+    };
+  
+    delete req.body.user_id;
+    if (req.file !== undefined) {
+      
+      Post.findOne({ _id: req.params.id })
         .then((post) => {
-            if (post == null) {
-                res.status(404).json({ message: "Le post n'existe pas" });
-            } else if ((post.userId == req.auth.userId) || req.auth.isAdmin ) {
-                const filename = post.imageUrl.split('/images/')[1];
-                fs.unlink(`images/${filename}`, () => {
-                    Post.updateOne({ _id: req.params.id }, {...postObject, _id: req.params.id })
-                        .then(() => res.status(200).json({ message: 'Objet supprimé' }))
-                        .catch(error => res.status(400).json({ error }));
-                });
-            } else {
-                res.status(403).json({ message: "Vous n'êtes pas autorisé à modifier ce post" });
-            }
-        });
-}
+          //On supprime l'image du post
+          const filename = post.imageUrl.split("/images/")[1];
+          fs.unlink(`images/${filename}`, () => {
+            //et on met à jour la DB avec une nouvelle image
+            Post.updateOne(
+              { _id: req.params.id },
+              { ...postObject, _id: req.params.id }
+            )
+              .then(() => res.status(200).json("Post mis à jour !"))
+              .catch((error) => res.status(401).json({ error }));
+          });
+        })
+        .catch((error) => res.status(404).json({ error }));
+    } else {
+      //Si pas d'image dans le post
+      Post.updateOne({ _id: req.params.id }, { description : description })
+        .then(() => res.status(200).json("Post mis à jour !"))
+        .catch((error) => res.status(401).json({ error }));
+    }
+  };
+
 
 // Supprimer un post
 
@@ -147,61 +129,3 @@ exports.likePost = async (req, res) => {
       }
     
   };
-
-// exports.likePost = (req, res, next) => {
-//     // Récupération du post avec params.id
-//     Post.findOne({ _id: req.params.id })
-//         .then(post => {
-//             switch (req.body.like) {
-//                 // Cas #1 : si l'utilisateur dislike le post
-//                 case -1:
-//                     Post.updateOne({ _id: req.params.id }, {
-//                             $inc: { dislikes: 1 },
-//                             $push: { usersDisliked: req.body.userId },
-//                             _id: req.params.id
-//                         })
-//                         .then(() => res.status(201).json({ message: 'Mince, nous n\'aimez plus ce post' }))
-//                         .catch(error => res.status(400).json({ error }))
-//                     break;
-
-//                     // Cas #2 : si la valeur like ou dislike est différente de 0
-//                 case 0:
-//                     // Cas #2-A : si le post est déjà liké
-//                     if (post.usersLiked.find(user => user === req.body.userId)) {
-//                         Post.updateOne({ _id: req.params.id }, {
-//                                 $inc: { likes: -1 },
-//                                 $pull: { usersLiked: req.body.userId },
-//                                 _id: req.params.id
-//                             })
-//                             .then(() => res.status(201).json({ message: 'Votre avis a bien été modifié, merci' }))
-//                             .catch(error => res.status(400).json({ error }))
-//                     }
-
-//                     // Cas #2-B : Si le post est déjà disliké
-//                     if (post.usersDisliked.find(user => user === req.body.userId)) {
-//                         Post.updateOne({ _id: req.params.id }, {
-//                                 $inc: { dislikes: -1 },
-//                                 $pull: { usersDisliked: req.body.userId },
-//                                 _id: req.params.id
-//                             })
-//                             .then(() => res.status(201).json({ message: 'Votre avis a bien été modifié, merci' }))
-//                             .catch(error => res.status(400).json({ error }));
-//                     }
-//                     break;
-
-//                     // Cas #3 : si l'utilisateur like le post
-//                 case 1:
-//                     Post.updateOne({ _id: req.params.id }, {
-//                             $inc: { likes: 1 },
-//                             $push: { usersLiked: req.body.userId },
-//                             _id: req.params.id
-//                         })
-//                         .then(() => res.status(201).json({ message: 'Super, vous adorez ce post!' }))
-//                         .catch(error => res.status(400).json({ error }));
-//                     break;
-//                 default:
-//                     return res.status(500).json({ messge :'erreur 500' });
-//             }
-//         })
-//         .catch(error => res.status(500).json({ error }))
-// }
